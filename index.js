7fd412b0756eb0b6a3a84fcf2c7475559975d2a9
LucasTapa <lucas.dfa@live.com>
2022-05-16 17:07:53 -0300

    requisito 1 feito

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const FILENAME = 'talker.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res, _next) => {
+    const data = await fs.readFile(FILENAME, 'utf8');
+    if (data) {
+      return res.status(200).json(JSON.parse(data));
+    }
+    return res.status(200).json(JSON.parse([]));
 });
 
 app.listen(PORT, () => {
