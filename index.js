c3b5f00fa0561ecb81b88a23eebb214bc23e4b1d
LucasTapa <lucas.dfa@live.com>
2022-05-17 13:44:40 -0300

    requisito 6 e 7 feito

--- index.js
+++ index.js
@@ -55,6 +55,34 @@ validationRate, async (req, res, _next) => {
   return res.status(201).json(newUser);
 });
 
+app.put('/talker/:id', authToken, validationName, validationAge, validationTalk, validationDate,
+validationRate, async (req, res, _next) => {
+  const { id } = req.params;
+  const { name, age, talk } = req.body;
+  const editTalker = { id: Number(id), name, age, talk };
+  const talkers = await readArq();
+
+  const editUser = talkers.find((talker) => talker.id === Number(id));
+
+  talkers.splice(editUser, 1, editTalker);
+  await writeArq(talkers);
+  res.status(200).json(editTalker);
+});
+
+app.delete('/talker/:id', authToken, async (req, res, _next) => {
+  const { id } = req.params;
+  const talkers = await readArq();
+
+  const talkerIndex = talkers.findIndex((talker) => talker.id === Number(id));
+
+  if (talkerIndex === -1) {
+    return res.status(404).json({ message: 'Talker não encontrado' });
+  }
+  await writeArq(talkerIndex);
+  talkers.splice(talkerIndex, 1);
+  res.status(204).end();
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
